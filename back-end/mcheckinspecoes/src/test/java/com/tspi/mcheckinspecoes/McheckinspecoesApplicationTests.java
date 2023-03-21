b06271d9ce2826f5ea41f596197d13af162a236b
Arthur Santos <ajsantos5d@gmail.com>
2023-03-21 19:33:52 -0300

    first commit

---
+++ back-end/mcheckinspecoes/src/test/java/com/tspi/mcheckinspecoes/McheckinspecoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tspi.mcheckinspecoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McheckinspecoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
